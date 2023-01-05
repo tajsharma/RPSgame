f20b38e19a82acd6ecb1fc5d0b4676bc2fd180ba
Taj <sharma.tajinder01@gmail.com>
2023-01-04 19:48:37 -0800

    Turn the player choice string entry to all lower case letters

--- logic.js
+++ logic.js
@@ -1,16 +1,16 @@
 function match(compChoice, userChoice){
-    return compChoice, userChoice
+    return userChoice
 }
 
 function computer(){
-    let listofOptions = ['Rock','Paper', 'Scissors']
-    let random = Math.floor(Math.random() * 3) // returns either 0,1, or 2
-    return listofOptions[random]
-}
+    let listofOptions = ['Rock','Paper', 'Scissors'];
+    let random = Math.floor(Math.random() * 3); // returns either 0,1, or 2
+    return listofOptions[random];
+};
 
-let compChoice = computer().toLowerCase()
+let compChoice = computer().toLowerCase();
 
-let playerChoice = prompt("Rock, Paper or Scissors? What do u pick!: ")
+var playerChoice = prompt("Rock, Paper or Scissors? What do u pick!: ");
 
-console.log(match(compChoice,playerChoice))
+console.log(match(compChoice,playerChoice.toLowerCase()));
 
