14bd88cd0c3f2ab51a77d471f14517ad6bd30398
Taj <sharma.tajinder01@gmail.com>
2023-01-04 19:39:24 -0800

    Add line to get random integer via using floor and random between 3

--- logic.js
+++ logic.js
@@ -4,9 +4,9 @@ function match(compChoice, userChoice){
 
 function computer(){
     let listofOptions = ['Rock','Paper', 'Scissors']
-    let random = Math.random(2)
+    let random = Math.floor(Math.random() * 3) // returns either 0,1, or 2
     return listofOptions[random]
 }
 
-let name = computer()
-console.log(name)
+let name1 = computer()
+console.log(name1)
