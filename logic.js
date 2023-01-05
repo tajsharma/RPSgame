64be759aa3c4276968aa470c7c713be6916a251d
Taj <sharma.tajinder01@gmail.com>
2023-01-04 19:10:39 -0800

    Create an array that contains all choices and returns a random one

---
+++ logic.js
@@ -0,0 +1,9 @@
+function match(compChoice, userChoice){
+
+}
+
+function computer(){
+    let listofOptions = ['Rock','Paper', 'Scissors']
+    let random = Math.random(2)
+    return listofOptions[random]
+}
