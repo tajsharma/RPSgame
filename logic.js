5ae3723f7c7da6156a02bb32f57966d3cc77a44c
Taj <sharma.tajinder01@gmail.com>
2023-01-04 20:01:27 -0800

    add if statement to check if user entry is valid

--- logic.js
+++ logic.js
@@ -1,16 +1,22 @@
 function match(compChoice, userChoice){
-    return userChoice
+    let winner = "Tie"
+    if(userChoice != 'rock' || userChoice!= 'paper' || userChoice != 'scissors'){
+        userChoice = prompt("Rock, Paper or Scissors? What do u pick!: ");
+    }else{
+        console.log('Valid choice')
+    }
 }
 
+let listofOptions = ['Rock','Paper', 'Scissors'];
+
 function computer(){
-    let listofOptions = ['Rock','Paper', 'Scissors'];
     let random = Math.floor(Math.random() * 3); // returns either 0,1, or 2
     return listofOptions[random];
 };
 
 let compChoice = computer().toLowerCase();
-
 var playerChoice = prompt("Rock, Paper or Scissors? What do u pick!: ");
+
 
 console.log(match(compChoice,playerChoice.toLowerCase()));
 
