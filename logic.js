86a29318f494f6becc6f6bad1d4cf1fb17494201
Taj Sharma <sharma.tajinder01@gmail.com>
2024-02-07 21:44:26 -0800

    Change the logic for the game to an if, isntead of for loop

--- logic.js
+++ logic.js
@@ -1,3 +1,45 @@
+let gameCount = 0; // Global variable to keep track of the number of games played
+let userScore = 0;
+let compScore = 0;
+
+function playMatch(userChoice) {
+    // Check if the game count is less than 10
+    if (gameCount < 10) {
+        let winner = match(userChoice);
+
+        if (winner == 'User'){
+            userScore++
+        }else if(winner=='Computer'){
+            compScore++
+        }
+            
+        // Create a new paragraph element to hold the result
+        let resultParagraph = document.createElement('p');
+        // Set the text content of the paragraph to the result
+        resultParagraph.textContent = winner + " wins!";
+        // Append the new paragraph to the container
+        document.querySelector('#resultText').appendChild(resultParagraph);
+        // Increment the game count
+        gameCount++;
+    } else {
+        // If the game count reaches 10, alert the user
+        let finalWinner = whoWon(userScore,compScore)
+        document.querySelector('#finalResults').innerText = finalWinner + " is the winner!"
+        alert("You have played 10 games already!");
+        console.log(userScore, compScore)
+    }
+}
+
+function whoWon(userScore,compScore){
+    if(userScore > compScore){
+        return "User"
+    }else if(userScore < compScore){
+        return "Computer"
+    }else{
+        return "No one"
+    }
+}
+
 function match(userChoice){
 
     let compChoice = computer().toLowerCase();
@@ -23,9 +65,7 @@ function match(userChoice){
     }else{
         winner = "No one";
     }
-
-    document.querySelector('#resultText').innerText = winner + " wins!";
-    console.log(winner," wins!");
+    return winner
 }
 
 let listofOptions = ['Rock','Paper', 'Scissors'];
